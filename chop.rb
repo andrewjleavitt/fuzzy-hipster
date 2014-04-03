a46333e4da1e08c515ed3babec45bc039cf6654e
Andy Leavitt <andy.l@truhearing.com>
2014-04-03 17:49:48 -0600

    fixed bug where matching cursor instead of the value at array[cursor]

--- chop.rb
+++ chop.rb
@@ -8,24 +8,26 @@ end
 
 
 def chop(target,sorted_array)
+  cursor = (sorted_array.length/2).floor
+  puts sorted_array
   puts "searching for " + target.to_s + " in list of " + sorted_array.length.to_s + " items."
-  cursor = sorted_array[sorted_array.length/2]
-  puts "cursor is " + cursor.to_s
-  if target == cursor
+  puts "value of cursor is " + cursor.to_s
+  puts "value of array at cursor is: " + sorted_array[cursor].to_s
+ 
+  if target == sorted_array[cursor]
   	return "I'm done. I found " + target.to_s + "."
-  elsif target < cursor
+  
+  elsif target < sorted_array[cursor]
   	puts "target is less than cursor"
   	chop(target, sorted_array.slice(0...cursor))
-  elsif target > cursor
-  	puts "target is greater than cursor"
-  	chop(target, sorted_array.slice(cursor...sorted_array.last))
+  
   else
-  	return "I'm not sure if it is here"
+  	puts "target is greater than cursor"
+  	chop(target, sorted_array.slice(cursor...sorted_array[-1]))
   end
 
 end
 
-list = gen_list 100
+list = gen_list 100000
 
-puts chop(51, list)
-# puts chop(list.sample, list)
+puts chop(list.sample, list)
