2a23208b7b927892886c2cee46135bb35388c4a1
Andy Leavitt <andyleavitt@gmail.com>
2014-04-04 09:10:51 -0600

    make it more betterer

--- chop.rb
+++ chop.rb
@@ -6,28 +6,22 @@ def gen_list(count)
   list
 end
 
-
-def chop(target,sorted_array)
+def chop(target,sorted_array,count)
   cursor = (sorted_array.length/2).floor
-  # puts sorted_array
   puts "searching for " + target.to_s + " in list of " + sorted_array.length.to_s + " items."
   puts "value of cursor is " + cursor.to_s
   puts "value of array at cursor is: " + sorted_array[cursor].to_s
- 
   if target == sorted_array[cursor]
-  	return "I'm done. I found " + target.to_s + "."
-  
+  	return "I'm done. I found " + target.to_s + ". It took me " + count.to_s + " steps."
+
   elsif target < sorted_array[cursor]
   	puts "target is less than cursor"
-  	chop(target, sorted_array.slice(0...cursor))
-  
+  	chop(target, sorted_array.slice(0...cursor), count+1)
   else
   	puts "target is greater than cursor"
-  	chop(target, sorted_array.slice(cursor...sorted_array[-1]))
+  	chop(target, sorted_array.slice(cursor...sorted_array[-1]),count+1)
   end
-
 end
 
-list = gen_list 10000000
-
-puts chop(list.sample, list)
+list = gen_list 1000000000
+puts chop(list.sample, list, 1)
--- fib.rb
+++ fib.rb
@@ -4,4 +4,8 @@ def fib(x)
   fib(x - 1) + fib(x - 2)
 end
 
-fib 10
+puts fib 10
+
+fib_l = -> (x){ x<=1 ? x : fib_l[x-1] + fib_l[x-2]}
+
+puts fib_l[10]
